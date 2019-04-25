0d2ef9dff42643a82c8fc00d6e809ec87fab1f4a
DongGyu96 <crow4079@naver.com>
2019-04-25 16:58:52 +0900

    20190425

--- STL Header/String.h
+++ STL Header/String.h
@@ -8,14 +8,17 @@ class String {
 	char* p{ nullptr };
 public:
 	String() = default;
-
 	String(int n);
-
 	String(const String& other);
-
 	String& operator=(const String& other);
-
 	~String();
+
+	// 이동을 지원하는 클래스
+	// 이동을 지원하기 위해서는 인자를 건드려야 함
+	// 이동생성자
+	String(String&& other);
+	// 이동할당연산자
+	String& operator=(String&& other);
 
 	void set(int n);
 
@@ -23,4 +26,8 @@ public:
 	char* getP() const;
 
 	friend ostream& operator<<(ostream&, const String&);
+
+
+	bool operator==(const String& other);
+	bool operator<(const String& rhs);
 };
